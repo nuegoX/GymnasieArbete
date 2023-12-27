491144cd736622e74193a568dc3a8a3ee2e040a6
Hugo <nuego@proton.me>
2023-12-27 15:41:20 +0100

    1

--- One Night at NTIschool/Assets/movement.cs
+++ One Night at NTIschool/Assets/movement.cs
@@ -1,65 +1,31 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class movement : MonoBehaviour
-{
-
-    #region "Variables"
-    public Rigidbody Rigid;
-    public float MouseSensitivity;
-    public float MoveSpeed;
-    public float JumpForce;
-    #endregion
-
-    void Update()
-    {
-        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
-        Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
-        if (Input.GetKeyDown("space"))
-            Rigid.AddForce(transform.up * JumpForce);
-    }
-}
-*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 100f;
+    private float speed = 2.0f;
+    public GameObject character;
 
     void Update()
     {
-        Move();
-        Turn();
-    }
 
-    void Move()
-    {
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.Self);
-
-        // Stop the sphere if it collides with something
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.5f))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            if (hit.collider.CompareTag("Wall"))
-            {
-                Rigidbody rb = GetComponent<Rigidbody>();
-                rb.velocity = Vector3.zero;
-                rb.angularVelocity = Vector3.zero;
-            }
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
         }
     }
-
-    void Turn()
-    {
-        float mouseX = Input.GetAxis("Mouse X");
-        transform.Rotate(Vector3.up * mouseX * rotationSpeed * Time.deltaTime);
-    }
 }
+
