9e4d8f55488ac494b0fc53b6e98291309dfc8f0c
Hugo <nuego@proton.me>
2024-02-16 17:00:59 +0100

    1

--- One Night at NTIschool/Assets/Rotate.cs
+++ One Night at NTIschool/Assets/Rotate.cs
@@ -1,12 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public float rotationSpeed = 3f; // Adjust the speed as needed
+    public float rotationSpeed = 30f; // Adjust the speed as needed
+    public bool AlarmsActivated = false;
 
-    void Update()
+    [SerializeField]
+    private Light[] pointLights;
+
+    public void Update()
     {
-        // Rotate the object continuously around its up axis (Y-axis)
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        if (AlarmsActivated)
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+            // Update intensity of selected point lights
+            foreach (Light light in pointLights)
+            {
+                if (light != null)
+                {
+                    // Interpolate intensity from 0 to 2 over time
+                    light.intensity = Mathf.Lerp(0f, 2f, Mathf.PingPong(Time.time, 1f));
+                }
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ActivateAlarm(10f));
+    }
+
+    private IEnumerator ActivateAlarm(float delayInSeconds)
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        AlarmsActivated = true;
     }
 }
